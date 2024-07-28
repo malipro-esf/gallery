ceb0a5a213eda4a9cc15f0da423c48dcf8253370
malipro-esf <m.alirezaei.pro@gmail.com>
2024-07-28 12:42:33 +0300

    change comment system

--- database/migrations/2022_08_14_224255_create_comments_table.php
+++ database/migrations/2022_08_14_224255_create_comments_table.php
@@ -15,12 +15,14 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->integer('artwork_id');
+            $table->integer('commentable_id');
+            $table->string('commentable_type');
             $table->string('user_fullname');
             $table->integer('user_id')->nullable();
             $table->string('email');
-            $table->text('message');
-            $table->text('reply');
+            $table->text('comment');
+            $table->text('reply')->nullable();
+            $table->integer('parent_id')->nullable();
             $table->text('ip_address')->nullable();
             $table->text('user_agent')->nullable();
             $table->enum('verification_status',['0','1'])->default('0');
